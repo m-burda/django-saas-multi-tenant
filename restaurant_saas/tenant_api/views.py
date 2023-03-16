434b89338a87f6480441b64cf362338175471b8b
EFFORTLS <114338792+EFFORTLS@users.noreply.github.com>
2023-03-16 11:16:47 +0200

    cleanup

--- restaurant_saas/tenant_api/views.py
+++ restaurant_saas/tenant_api/views.py
@@ -1,38 +1,26 @@
 from rest_framework.generics import RetrieveAPIView
 from restaurant.models import MenuModel, CategoryModel, MenuItemModel
-from restaurant.serializers import MenuSerializer, CategorySerializer, MenuItemSerializer
+from restaurant.serializers import (
+    MenuSerializer,
+    CategorySerializer,
+    MenuItemSerializer,
+)
 from .serializers import TenantMenuSerializer, TenantCategorySerializer
 
 
 class TenantMenuView(RetrieveAPIView):
     queryset = MenuModel.objects.all()
     serializer_class = TenantMenuSerializer
-    lookup_field = 'id'
+    lookup_field = "id"
 
 
 class TenantCategoryView(RetrieveAPIView):
     queryset = CategoryModel.objects.all()
     serializer_class = TenantCategorySerializer
-    lookup_field = 'id'
+    lookup_field = "id"
 
 
 class TenantItemView(RetrieveAPIView):
     queryset = MenuItemModel.objects.all()
     serializer_class = MenuItemSerializer
-    lookup_field = 'id'
-
-    # def get_queryset(self):
-    #     menu_id = self.kwargs['id']
-    #     return MenuModel.objects.filter(id=menu_id)
-
-
-# class TenantCategoryView(RetrieveAPIView):
-#     queryset = CategoryModel.objects.all()
-#     serializer_class = CategorySerializer
-#     lookup_field = 'id'
-#
-#
-# class TenantMenuItemView(RetrieveAPIView):
-#     queryset = MenuItemModel.objects.all()
-#     serializer_class = MenuItemSerializer
-#     lookup_field = 'id'
+    lookup_field = "id"
