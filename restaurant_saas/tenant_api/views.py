14573e85850a895e14ef6f81a892bd1d8e77d888
EFFORTLS <114338792+EFFORTLS@users.noreply.github.com>
2023-03-16 09:59:59 +0200

    Add README

--- restaurant_saas/tenant_api/views.py
+++ restaurant_saas/tenant_api/views.py
@@ -1,16 +1,9 @@
-from django.shortcuts import render
-# from .serializers import MenuSerializer, CategorySerializer, MenuItemSerializer
-from rest_framework.decorators import action
-from rest_framework.routers import SimpleRouter
-
 from rest_framework.generics import RetrieveAPIView
 from restaurant.models import MenuModel, CategoryModel, MenuItemModel
-from restaurant.views import MenuViewSet
-from .serializers import TenantMenuSerializer
 from restaurant.serializers import MenuSerializer
 
 
 class TenantMenuView(RetrieveAPIView):
     queryset = MenuModel.objects.all()
-    serializer_class = TenantMenuSerializer
+    serializer_class = MenuSerializer
     lookup_field = 'id'
