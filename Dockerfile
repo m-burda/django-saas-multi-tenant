84a84d9789eb726b6aff9d13d6c997efff73e480
EFFORTLS <maofspin@gmail.com>
2023-03-18 03:20:55 +0200

    deploy

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.10.10-slim-buster
+#SHELL ["bin/bash", "-c"]
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+#ENV DEBUG 0
+
+RUN mkdir /django
+WORKDIR /django
+COPY requirements.txt /django/
+
+RUN apt-get update && apt-get -y install postgresql libpq-dev postgresql-client postgresql-client-common python3-pip git netcat
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+WORKDIR ./restaurant_saas
+
+#CMD gunicorn restaurant_saas.wsgi:application --bind 0.0.0.0:8000
+CMD gunicorn restaurant_saas.wsgi:application --bind 0.0.0.0:$PORT
--- restaurant_saas/restaurant_saas/settings.py
+++ restaurant_saas/restaurant_saas/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG', True))
 
 ALLOWED_HOSTS = []
 
@@ -117,8 +117,8 @@ DATABASES = {
         'NAME': 'multitenant_restaurants',
         'USER': str(os.getenv('POSTGRES_USER')),
         'PASSWORD': str(os.getenv('POSTGRES_PASS')),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'HOST': str(os.getenv('DB_HOST')),
+        'PORT': int(os.getenv('DB_PORT')),
     }
 }
 
@@ -165,7 +165,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
