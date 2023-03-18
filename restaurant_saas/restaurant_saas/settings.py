d983e45e95e2a479858900d15443f2e9fd66b3d4
EFFORTLS <maofspin@gmail.com>
2023-03-18 18:36:02 +0200

    add subdomain wildcard

--- restaurant_saas/restaurant_saas/settings.py
+++ restaurant_saas/restaurant_saas/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
-from datetime import timedelta
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv('DEBUG', False))
@@ -87,7 +87,7 @@ REST_FRAMEWORK = {
 ROOT_URLCONF = 'restaurant_saas.urls'
 PUBLIC_SCHEMA_URLCONF = 'restaurant_saas.urls_public'
 
-SHOW_PUBLIC_IF_NO_TENANT_FOUND = True
+SHOW_PUBLIC_IF_NO_TENANT_FOUND = False
 LOGIN_REDIRECT_URL = ''
 
 TEMPLATES = [
